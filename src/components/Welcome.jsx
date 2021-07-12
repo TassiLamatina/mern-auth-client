f09479a4f76510b2a1b34c3aa861053a46dc4d13
funbunch <shannan_bunch@hotmail.com>
2021-07-12 15:45:43 -0700

    added landing page form

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,25 @@
 export default function Welcome() {
     return(
-        <div>
-            hello from Welcome
+        <div className="container"> 
+        <div className="col-md-9">
+          <img src="https://placedog.net/600" alt="" />
         </div>
+        <div className="col-md-3">
+          <form id="sign-up" method="POST" action="/user">
+            <div class="mb-3">
+              <label class="form-label">Username:</label>
+              <input class="form-control" type="text" name="username"/>
+            </div>
+            <div class="mb-4">
+              <label class="form-label">Zipcode:</label>
+              <input class="form-control" type="number" name="zipcode"/>
+            </div>
+            <input class="btn" type="submit" />
+          </form>
+        </div>
+  
+        <p>{message}</p>
+      
+      </div>
     )
 }
